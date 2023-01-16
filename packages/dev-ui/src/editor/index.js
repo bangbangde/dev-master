d2f9b1329533e535c0e964e67beb634a65217ca2
赵成齐 <chengqifw@gmail.com>
2023-01-17 00:53:00 +0800

    update

--- packages/dev-ui/src/editor/index.js
+++ packages/dev-ui/src/editor/index.js
@@ -3,22 +3,22 @@ import { genKey } from "./utils";
 const createOperation = {
   insertText: (path, text) => ({ type: 'insert_text', path, text }),
   removeText: (path, offset, backword) => ({ type: 'remove_text', path, offset, backword }),
-  insertNode: (path, node) => ({}),
+  insertNode: (path, offset, node) => ({}),
   splitNode: (path) => ({}),
   mergeNode: (range) => ({}),
   setNode: (path, property) => ({ type: 'set_node', property })
 }
 
 export const createEditor = () => {
+  let content = null;
+  let selection = null;
+  let operations = [];
+
   const editor = {
     key: genKey(),
-    content: null,
-    selection: null,
-    operations: [],
     command: {
-      insertText() {},
-      insertNode() {},
-      setNode() {}
+      insertText: text => {},
+      insertNode: node => {}
     },
     setContent(content) {
 

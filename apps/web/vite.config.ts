dd29a4b08cc5279d8e869d77a898d9ad0b1e7ed7
Frank <chengqifw@gmail.com>
2023-02-15 11:04:57 +0800

    update

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/szb",
+  base: "/dev-master",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {

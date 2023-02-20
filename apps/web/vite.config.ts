f7fa29e0679f55d27736c4e839e45534a11df17e
Frank <chengqifw@gmail.com>
2023-02-20 12:22:58 +0800

    update

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+
+const env = loadEnv("development", process.cwd(), "VITE_");
+console.log("env", process.cwd(), env);
 
 // https://vitejs.dev/config/
 export default defineConfig({

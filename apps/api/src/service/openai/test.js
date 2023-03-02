213bf17e20a96185a2f92786392ff9c68c22c2dd
Frank <chengqifw@gmail.com>
2023-03-02 21:53:50 +0800

    upgrade openai

--- apps/api/src/service/openai/test.js
+++ apps/api/src/service/openai/test.js
@@ -6,15 +6,9 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: "hello",
-  temperature: 0.9,
-  max_tokens: 2048,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0.6,
-  stop: [" Human:", " AI:"],
+openai.createChatCompletion({
+  model: "gpt-3.5-turbo",
+  messages: [{role: "user", content: "Hello world"}],
 }).then(res => {
   try {
     console.log("chat response:", res.data.choices[0]);
@@ -23,3 +17,21 @@ openai.createCompletion({
   }
   return res;
 });
+
+// openai.createCompletion({
+//   model: "text-davinci-003",
+//   prompt: "hello",
+//   temperature: 0.9,
+//   max_tokens: 2048,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0.6,
+//   stop: [" Human:", " AI:"],
+// }).then(res => {
+//   try {
+//     console.log("chat response:", res.data.choices[0]);
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   return res;
+// });
